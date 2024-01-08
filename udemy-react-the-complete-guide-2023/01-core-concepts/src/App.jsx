53128e4dd01c11130cf8919f997d9671e70fb5e8
David Tsai (MTO) <Davidtsai@guoshipartners.com>
2024-01-08 10:29:31 +0800

    CoreConcept - Learning useState

--- udemy-react-the-complete-guide-2023/01-core-concepts/src/App.jsx
+++ udemy-react-the-complete-guide-2023/01-core-concepts/src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import CoreConcepts from "./components/CoreConcepts/CoreConcepts";
 import Header from "./components/Header/Header";
 import TabButton from "./components/TabButton";
 import { coreConceptsData } from "../data/data";
+import React from "react";
+
+const tabBtnData = ["components", "jsx", "props", "state"];
 
 function App() {
+  const [btnContent, setBtnContent] = useState("Please Select a Subject");
+  // btnContent = 'Please Select a Subject'
+  function handleSelect(selectedBtn) {
+    tabBtnData.forEach((item) => {
+      if (item === selectedBtn) {
+        console.log(`${item} is being clicked!`);
+        setBtnContent(item)
+      }
+    });
+  }
   return (
     <>
       <Header />
@@ -22,9 +36,30 @@ function App() {
           <section id="examples">
             <h2>Examples</h2>
             <menu>
-              {/* Learning Children Props */}
-              <TabButton>Components</TabButton>
+              {/* Learning Children Props and passing function as props parameters */}
+              <TabButton
+                onSelect={function () {
+                  handleSelect(tabBtnData[0]);
+                }}
+              >
+                Components
+              </TabButton>
+              <TabButton
+                onSelect={function () {
+                  handleSelect(tabBtnData[1]);
+                }}
+              >
+                JSX
+              </TabButton>
+              <TabButton onSelect={() => handleSelect(tabBtnData[2])}>
+                Props
+              </TabButton>
+              <TabButton onSelect={() => handleSelect(tabBtnData[3])}>
+                State
+              </TabButton>
             </menu>
+            {/* Dynamic Content */}
+            {btnContent}
           </section>
         </main>
       </div>
--- udemy-react-the-complete-guide-2023/01-core-concepts/src/components/TabButton.jsx
+++ udemy-react-the-complete-guide-2023/01-core-concepts/src/components/TabButton.jsx
@@ -1,9 +1,8 @@
-const tabButtonList = ['component', 'bla', 'bla']
-
-export default function TabButton({children}) {
-    return (
-        <li>
-            <button>{children}</button>
-        </li>
-    )
+export default function TabButton({ onSelect, children }) {
+  return (
+    <li>
+        {/* passing handleSelect as props parameter by the name onSelect */}
+      <button onClick={onSelect}>{children}</button>
+    </li>
+  );
 }
