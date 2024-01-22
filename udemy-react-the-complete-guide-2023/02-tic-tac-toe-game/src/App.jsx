438fdc5ad6c38c2ac3d88aca5bc604f905799c2f
David Tsai <>
2024-01-23 00:11:44 +0800

    Starting tic tac toe game with reusable component (player)

--- udemy-react-the-complete-guide-2023/02-tic-tac-toe-game/src/App.jsx
+++ udemy-react-the-complete-guide-2023/02-tic-tac-toe-game/src/App.jsx
@@ -1,10 +1,17 @@
+import Player from './Components/Player'
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player player="player 1" symbol="X"/>
+          <Player player="player 2" symbol="O"/>
+        </ol>
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
