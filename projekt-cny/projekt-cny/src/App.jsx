9f65ba61e9ce812b0dfc1bfdef3f1ac7e50f514e
David Tsai <tsaitingyudavid@gmail.com>
2024-02-11 21:41:16 +0800

    Working on how to use g maps api in react

--- projekt-cny/projekt-cny/src/App.jsx
+++ projekt-cny/projekt-cny/src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// studying google maps api in react from: https://developers.google.com/maps/documentation/javascript/react-map?hl=zh-tw#javascript_1
 
-function App() {
-  const [count, setCount] = useState(0)
+import "bootstrap/dist/css/bootstrap.min.css";  // Bootstrap CSS
+import "bootstrap/dist/js/bootstrap.bundle.min";  // Bootstrap Bundle JS
+import React from "react";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { useState, useRef } from "react";
+import "./App.css";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const render = (status) => {
+  return <h1>{status}</h1>;
 }
 
-export default App
+function App() {
+  // const Map:React.FC<{}> = () => {}
+  const Map = () => {}
+
+  const ref = React.useRef(null);
+  const [map, setMap] = React.useState();
+
+
+  React.useEffect(() => {
+    if (ref.current && !map) {
+      setMap(new window.google.maps.Map(ref.current, {}));
+    }
+  }, [ref, map]);
+
+
+  return (
+    <div className="page-wrapper">
+      <header className="header-wrapper">
+        <div className="announcement">
+          <a href="">請支持貓咖，讓貓咖一起變得更好</a>
+        </div>
+        <div className="main-header">
+          <a href="" className="main-logo">
+            貓咖 | Cat Cafe
+          </a>
+          <a href="" className="main-tutorial">
+            第一次找貓咖？
+          </a>
+          <div className="menu-container">
+            <button type="button" className="btn btn-dark btn-lg">
+              註冊
+            </button>
+            <button type="button" className="btn btn-light btn-lg">
+              登入
+            </button>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className="user-search-container">
+          <div className="user-search-title-container">
+            <h3>工作 + 吸貓 + 咖啡？</h3>
+            <h4>快來找最適合你的貓咖！</h4>
+            <div className="user-search-input-container">
+              <input type="text" placeholder="輸入縣市、地區或店名" />
+              <span className="material-symbols-outlined"> search </span>
+            </div>
+            <div className="user-search-neighborhood-container">
+              <div className="user-search-neighborhood-container-text-container">
+                <h4>找找附近的貓咖？</h4>
+                <p>需開啟瀏覽器定位權限</p>
+              </div>
+              <button type="button" className="btn btn-light btn-md">
+                立即搜尋
+              </button>
+            </div>
+          </div>
+        </section>
+        <section className="map-container">
+          <Wrapper
+            apiKey={import.meta.env.MY_GOOGLE_MAPS_API_KEY}
+            render={render}
+          />
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ projekt-cny/projekt-cny/src/components/MyMaps.jsx
@@ -0,0 +1,3 @@
+export default function MyMaps() {
+  return <div id="map"></div>;
+}
