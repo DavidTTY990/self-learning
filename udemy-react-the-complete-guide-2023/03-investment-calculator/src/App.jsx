fef9827e79149ca8138ba40d12e792823f69c6cf
David Tsai <tsaitingyudavid@gmail.com>
2024-02-16 18:49:12 +0800

    Working on udemy calculator

--- udemy-react-the-complete-guide-2023/03-investment-calculator/src/App.jsx
+++ udemy-react-the-complete-guide-2023/03-investment-calculator/src/App.jsx
@@ -1,7 +1,45 @@
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <header id="header">
+        <img src="/investment-calculator-logo.png" alt="" />
+        <h1>React Investment Calculator</h1>
+      </header>
+      <div id="user-input">
+        <div className="input-group">
+          <div>
+            <label htmlFor="">innitial investment</label>
+            <input type="number" />
+          </div>
+          <div>
+            <label htmlFor="">annual investment</label>
+            <input type="number" />
+          </div>
+        </div>
+        <div className="input-group">
+          <div>
+            <label htmlFor="">expected return</label>
+            <input type="number" />
+          </div>
+          <div>
+            <label htmlFor="">duration (year)</label>
+            <input type="number" />
+          </div>
+        </div>
+      </div>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+      </table>
+    </>
+  );
 }
 
-export default App
+export default App;
