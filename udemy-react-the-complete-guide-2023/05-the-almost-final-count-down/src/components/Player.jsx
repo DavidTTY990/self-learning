ef0b8f67a84f3107b06cc85a27f2e7b6e6d79ffc
David Tsai <tsaitingyudavid@gmail.com>
2024-03-20 16:50:52 +0800

    add firebase config to tic-tac-toe game

--- udemy-react-the-complete-guide-2023/05-the-almost-final-count-down/src/components/Player.jsx
+++ udemy-react-the-complete-guide-2023/05-the-almost-final-count-down/src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+
+  function handleChange(e) {
+    setIsSubmitted(false);
+    setPlayerName(e.target.value);
+  }
+
+  function handleClick() {
+    setIsSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {isSubmitted ? playerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
